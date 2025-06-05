23aa0af0f40d57ce0a6a4ea42062b869b05daca6
Rafael Z Tanganelli <rzamolo-@student.42madrid.com>
2025-06-05 16:45:45 +0200

    `env` working using ft_substr and ft_strdup, and freeing memory after using it. TODO: copy key and value to the structure

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: rzt <rzt@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 11:38:33 by rzt               #+#    #+#             */
-/*   Updated: 2025/06/05 16:33:16 by rzt              ###   ########.fr       */
+/*   Updated: 2025/06/05 16:43:11 by rzt              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	env(const char **envp)
 {
-	char	**split;
+	char	*firstequal;
 	char	*key;
 	char	*value;
-	int		i;
 
 	while (*envp)
 	{
-		split = ft_split(*envp, '=');
-		key = ft_strdup(split[0]);
-		value = ft_strdup(split[1]);
+		firstequal = ft_strchr(*envp, '=');
+		if (firstequal)
+		{
+		key = ft_substr(*envp, 0, firstequal - *envp);
+		value = ft_strdup(firstequal + 1);
+		
 		ft_putstr_fd(key, STDOUT_FILENO);
 		ft_putstr_fd("=", STDOUT_FILENO);
 		ft_putstr_fd(value, STDOUT_FILENO);
@@ -31,10 +33,7 @@ int	env(const char **envp)
 		
 		free(key);
 		free(value);
-		i = 0;
-		while (split[i])
-			free(split[i++]);
-		free(split);
+		}
 		envp++;
 	}
 	return (0);
