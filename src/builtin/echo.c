290cb6d4dbea2467d2abbdd117414f3a3774fb78
Rafael Z Tanganelli <kabelu@gmail.com>
2025-06-26 17:51:14 +0200

    Changing `echo` to interpret \t and \n

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rzt <rzt@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 11:38:39 by rzt               #+#    #+#             */
-/*   Updated: 2025/06/16 12:35:09 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2025/06/26 17:49:47 by rzt              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static void	print_with_escapes(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\' && str[i + 1])
+		{
+			if (str[i + 1] == 'n')
+			{
+				ft_putchar_fd('\n', STDOUT_FILENO);
+				i += 2;
+				continue ;
+			}
+			else if (str[i + 1] == 't')
+			{
+				ft_putchar_fd('\t', STDOUT_FILENO);
+				i += 2;
+				continue ;
+			}
+		}
+		ft_putchar_fd(str[i], STDOUT_FILENO);
+		i++;
+	}
+}
 
 static int	check_flag(char *arg)
 {
@@ -43,7 +70,7 @@ int	mini_echo(char *arg[])
 	}
 	while (arg && arg[i])
 	{
-		ft_putstr_fd(arg[i], STDOUT_FILENO);
+		print_with_escapes(arg[i]);
 		if (arg[i + 1])
 			ft_putstr_fd(" ", STDOUT_FILENO);
 		i++;
