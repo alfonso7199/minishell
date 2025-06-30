f1d26cf1ea1c3b0d33e51025ff4635a15d567da8
Rafael Z Tanganelli <kabelu@gmail.com>
2025-06-30 17:40:21 +0200

    PATH working, need to adjust SHLVL

--- src/executor/executor_external.c
+++ src/executor/executor_external.c
@@ -6,64 +6,28 @@
 /*   By: rzt <rzt@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 18:02:38 by rzt               #+#    #+#             */
-/*   Updated: 2025/06/30 10:56:21 by rzt              ###   ########.fr       */
+/*   Updated: 2025/06/30 17:19:29 by rzt              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	execute_external_cmd(t_cmd *cmd, t_shell *shell)
-{
-	pid_t	pid;
-	char	*cmd_path;
-
-	if (!cmd || !cmd->args || !cmd->args[0])
-		return (1);
-	cmd_path = find_command_path(cmd->args[0], shell->env);
-	if (!cmd_path)
-		return (handle_command_not_found(cmd->args[0]));
-	pid = fork();
-	if (pid == -1)
-		return (handle_fork_error());
-	if (pid == 0)
-		execute_child_process(cmd, cmd_path, shell);
-	return (handle_command_not_found(cmd->args[0]));
-}
-
-char	*find_command_path(char *cmd_name, t_env *envp)
-{
-	char	*path_env;
-	char	**paths;
-	char	*full_path;
-
-	if (!cmd_name)
-		return (NULL);
-	if (ft_strchr(cmd_name, '/'))
-		return (check_absolute_path(cmd_name));
-	path_env = get_env_value(envp, "PATH");
-	if (!path_env)
-		return (NULL);
-	paths = ft_split(path_env, ':');
-	if (!paths)
-		return (NULL);
-	full_path = search_in_paths(cmd_name, paths);
-	free_string_array(paths);
-	return (full_path);
-}
-
-char	*check_absolute_path(char *path)
+char	*validate_absolute_path(char *path)
 {
 	struct stat	path_stat;
 
-	if (stat(path, &path_stat) == 0)
-	{
-		if (S_ISREG(path_stat.st_mode) && (path_stat.st_mode & S_IXUSR))
-			return (path);
-	}
-	return (NULL);
+	if (access(path, F_OK) != 0)
+		return (NULL);
+	if (stat(path, &path_stat) != 0)
+		return (NULL);
+	if (!S_ISREG(path_stat.st_mode))
+		return (NULL);
+	if (access(path, X_OK) != 0)
+		return (NULL);
+	return (ft_strdup(path));
 }
 
-char	*search_in_paths(char *cmd_name, char **paths)
+char	*find_in_path_dirs(char *cmd_name, char **paths)
 {
 	int		i;
 	char	*temp_path;
@@ -85,6 +49,48 @@ char	*search_in_paths(char *cmd_name, char **paths)
 		i++;
 	}
 	return (NULL);
+}
+
+char	*get_command_path(char *cmd_name, t_env *envp)
+{
+	char	*path_env;
+	char	**paths;
+	char	*full_path;
+
+	if (!cmd_name)
+		return (NULL);
+	if (ft_strchr(cmd_name, '/'))
+		return (validate_absolute_path(cmd_name));
+	path_env = get_env_value(envp, "PATH");
+	if (!path_env)
+		return (NULL);
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		return (NULL);
+	full_path = find_in_path_dirs(cmd_name, paths);
+	free_string_array(paths);
+	return (full_path);
+}
+
+int	execute_external_cmd(t_cmd *cmd, t_shell *shell)
+{
+	char	*cmd_path;
+	pid_t	pid;
+	int		status;
+
+	cmd_path = get_command_path(cmd->args[0], shell->env);
+	if (!cmd_path)
+		return (handle_command_not_found(cmd->args[0]));
+	pid = fork();
+	if (pid == -1)
+	{
+		free(cmd_path);
+		return (handle_fork_error());
+	}
+	if (pid == 0)
+		execute_child_process(cmd, cmd_path, shell);
+	free(cmd_path);
+	return (wait_for_child(pid, &status));
 }
 
 void	execute_child_process(t_cmd *cmd, char *cmd_path, t_shell *shell)
