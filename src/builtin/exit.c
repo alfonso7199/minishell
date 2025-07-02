ff2007465015e78d0435615fc79d7751e5c9ba48
Rafael Z Tanganelli <kabelu@gmail.com>
2025-07-02 18:21:10 +0200

    Refactor mini_exit function to enhance argument validation and streamline exit code handling

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,36 +6,62 @@
 /*   By: rzt <rzt@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 11:38:08 by rzt               #+#    #+#             */
-/*   Updated: 2025/07/02 17:28:08 by rzt              ###   ########.fr       */
+/*   Updated: 2025/07/02 18:20:52 by rzt              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static void	error_numeric(char *arg, t_shell *shell)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	cleanup_shell(shell);
+	exit(2);
+}
+
+static long	parse_arg_value(char *s)
+{
+	long	nbr;
+	int		sign;
+	int		i;
+	int		digit;
+
+	nbr = 0;
+	sign = 1;
+	i = 0;
+	if (s[0] == '-' || s[0] == '+')
+	{
+		if (s[0] == '-')
+			sign = -1;
+		i++;
+	}
+	while (s[i])
+	{
+		digit = s[i] - '0';
+		nbr = nbr * 10 + digit;
+		i++;
+	}
+	return (nbr * sign);
+}
+
 int	mini_exit(t_cmd *cmd, t_shell *shell)
 {
-	int		exit_code;
+	long	exit_code;
 
-	exit_code = shell->exit_status;
-	if (cmd->args[1])
+	if (cmd->args[1] && !ft_isnbr(cmd->args[1]))
+		error_numeric(cmd->args[1], shell);
+	if (cmd->args[1] && cmd->args[2])
 	{
-		if (!ft_isnbr(cmd->args[1]))
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(cmd->args[1], STDERR_FILENO);
-			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-			cleanup_shell(shell);
-			exit(2);
-		}
-		if (cmd->args[2])
-		{
-			ft_putstr_fd("minishell: too many arguments\n",
-				STDERR_FILENO);
-			return (1);
-		}
-		exit_code = ft_atoi(cmd->args[1]);
+		ft_putstr_fd("minishell: too many arguments\n", STDERR_FILENO);
+		return (1);
 	}
-	ft_putstr_fd("exit\n", STDIN_FILENO);
+	if (cmd->args[1])
+		exit_code = parse_arg_value(cmd->args[1]);
+	else
+		exit_code = shell->exit_status;
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
 	cleanup_shell(shell);
-	exit(exit_code & 0xFF);
+	exit((unsigned char)exit_code);
 }
